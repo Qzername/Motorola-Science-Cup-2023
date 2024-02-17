6b93fbc64f61b01482d96ed026bf6f59a2100118
heapy <63428380+heapybruh@users.noreply.github.com>
2024-02-17 17:08:05 +0100

    Remove test obstacles in GameWindow

--- Tempest/GameWindow.cs
+++ Tempest/GameWindow.cs
@@ -13,18 +13,6 @@ namespace Tempest
 
             Instantiate(new MapManager());
             Instantiate(new Player());
-
-            var obstacle1 = new Spike();
-            obstacle1.Setup(0, 650f);           
-            Instantiate(obstacle1);
-
-            var obstacle2 = new Spike();
-            obstacle2.Setup(2, 750f);            
-            Instantiate(obstacle2);
-
-            var obstacle3 = new Spike();
-            obstacle3.Setup(6, 850f);           
-            Instantiate(obstacle3);
         }
 
         public override void Update(Canvas canvas)
